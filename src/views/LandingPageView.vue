f774d48fade60d8a1b5d09c2f18fef752c1ccaaf
Jace Roldan <jaceroldan16@gmail.com>
2022-03-12 19:51:16 +0800

    Migrate vanilla personal website

---
+++ src/views/LandingPageView.vue
@@ -0,0 +1,193 @@
+<template>
+    <main class="container">
+        <nav class="main-nav">
+            <div class="main-nav--body">
+                <div class="nav-logo">
+                    <!-- Img: For the logo -->
+                    <h1>Jace Roldan</h1>
+                </div>
+            </div>
+            <ul class="nav-menu anchored">
+                <li>About</li>
+                <li>Portfolio</li>
+                <li>Experience</li>
+            </ul>
+        </nav>
+        <section class="subsection banner-container">
+            <header class="banner-text">
+                <h1 class="heading">Jace Roldan</h1>
+                <h2 class="header-subtext">Web Developer</h2>
+            </header>
+            <aside class="askme">
+                <div class="askme-input" style="display: none;">
+                    <h3>Ask me?</h3>
+                    <p>Can Jace <span class="">do</span>:</p>
+                    <input type="text">
+                    <button>Check</button>
+                </div>
+                <div class="askme-result">
+                    <h3>Of course I can!</h3>
+                    <p>Send me an email at <a href="mailto:jaceroldan16@gmail.com" target="_blank">jaceroldan16@gmail.com!</a></p>
+                </div>
+            </aside>
+        </section>
+        <section class="subsection about-container">
+
+        </section>
+        <section class="subsection experience-container">
+            <div class="experience-header">
+                <h1>Experience</h1> 
+            </div>
+            <div class="experience-pane">
+                <ul class="experience-list anchored">
+                    <li>BPOSeats</li>
+                    <li>UP Computer Science Guild</li>
+                    <li>Tug-ani</li>
+                </ul>
+                <div class="experience-showcase">
+                    <div class="left">
+                        <div class="row">
+                            <div class="col">
+                                <div class="company-logo">
+                                    <img src="../assets/img/bposeats-logo-icon.png" alt="BPOSeats Logo">
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="work-experience">
+                                    <div class="company-exp">
+                                        <h2>Team Lead</h2>
+                                        <span><small>August 2019 - Present</small></span>
+                                        <p><a href="https://bposeats.com/info" target="_blank">BPOSeats.com</a></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="work-experience">
+                                    <div class="role-exp">
+                                        <p>BPOSeats is a seat-leasing company based in the Philippines.</p>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eget sodales metus.
+                                        Donec non consequat dui. Suspendisse sed massa et metus maximus tempus quis id lorem.
+                                        Vivamus sed nulla id leo condimentum bibendum eget at risus. Sed vestibulum suscipit 
+                                        elementum. Aenean augue metus, consequat ac aliquam eget, feugiat luctus nibh. Nam 
+                                        consequat, enim egestas ultricies lobortis, felis tortor aliquam sem, sit amet ornare 
+                                        est diam eu urna. Mauris vel convallis arcu, in lobortis orci. Morbi nec varius nibh. 
+                                        Fusce tempor condimentum hendrerit. Sed in enim id tortor bibendum auctor eu et magna. 
+                                        Nulla euismod ex sit amet nisl dapibus vulputate.</p>
+                                        <p>Duis consectetur neque at risus rhoncus, non rutrum velit euismod. Cras dictum felis 
+                                        purus, sit amet lacinia nisi congue sit amet. Aliquam blandit augue malesuada eleifend 
+                                        ultricies. Nunc sed purus vitae justo egestas tristique. Phasellus in nisi sed libero 
+                                        posuere semper ac et orci. Nulla tristique velit nec neque dictum, id sodales arcu 
+                                        posuere. Curabitur porta, nisi interdum sollicitudin semper, ligula nulla elementum 
+                                        massa, sed iaculis libero lorem sed erat. Nullam dictum nunc nec luctus semper. Quisque 
+                                        a risus ac arcu dictum ultricies id eget tortor. Vestibulum ante ipsum primis in 
+                                        faucibus orci luctus et ultrices posuere cubilia curae; Phasellus tortor ipsum, venenatis
+                                        at velit non, varius accumsan mauris. Nullam tincidunt erat in augue tempus sodales.
+                                        </p>
+                                        <p>Duis consectetur neque at risus rhoncus, non rutrum velit euismod. Cras dictum felis 
+                                            purus, sit amet lacinia nisi congue sit amet. Aliquam blandit augue malesuada eleifend 
+                                            ultricies. Nunc sed purus vitae justo egestas tristique. Phasellus in nisi sed libero 
+                                            posuere semper ac et orci. Nulla tristique velit nec neque dictum, id sodales arcu 
+                                            posuere. Curabitur porta, nisi interdum sollicitudin semper, ligula nulla elementum 
+                                            massa, sed iaculis libero lorem sed erat. Nullam dictum nunc nec luctus semper. Quisque 
+                                            a risus ac arcu dictum ultricies id eget tortor. Vestibulum ante ipsum primis in 
+                                            faucibus orci luctus et ultrices posuere cubilia curae; Phasellus tortor ipsum, venenatis
+                                            at velit non, varius accumsan mauris. Nullam tincidunt erat in augue tempus sodales.
+                                        </p>
+                                        <p>Duis consectetur neque at risus rhoncus, non rutrum velit euismod. Cras dictum felis 
+                                            purus, sit amet lacinia nisi congue sit amet. Aliquam blandit augue malesuada eleifend 
+                                            ultricies. Nunc sed purus vitae justo egestas tristique. Phasellus in nisi sed libero 
+                                            posuere semper ac et orci. Nulla tristique velit nec neque dictum, id sodales arcu 
+                                            posuere. Curabitur porta, nisi interdum sollicitudin semper, ligula nulla elementum 
+                                            massa, sed iaculis libero lorem sed erat. Nullam dictum nunc nec luctus semper. Quisque 
+                                            a risus ac arcu dictum ultricies id eget tortor. Vestibulum ante ipsum primis in 
+                                            faucibus orci luctus et ultrices posuere cubilia curae; Phasellus tortor ipsum, venenatis
+                                            at velit non, varius accumsan mauris. Nullam tincidunt erat in augue tempus sodales.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="left hidden">
+                        <div class="row">
+                            <div class="col">
+                                <div class="company-logo">
+                                    <img src="../assets/img/UP-Seal.png" alt="University of the Philippines logo">
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="work-experience">
+                                    <div class="company-exp">
+                                        <h2>President</h2>
+                                        <span><small>April 2017 - May 2019</small></span>
+                                        <p><a href="https://www.facebook.com/UPCSG" target="_blank">UP Computer Science Guild</a></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="work-experience">
+                                    <div class="role-exp">
+                                        <p>At the Computer Science Guild, I helped organize activities to support the
+                                        career development of fellow CS majors.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="left hidden">
+                        <div class="row">
+                            <div class="col">
+                                <div class="company-logo">
+                                    <img src="../assets/img/Tug-ani Logo.png" alt="BPOSeats Logo" height="100" width="45">
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="work-experience">
+                                    <div class="company-exp">
+                                        <h2>Associate Editor - Internal</h2>
+                                        <span><small>May 2017 - May 2019</small></span>
+                                        <p><a href="https://facebook.com/upcebutugani" target="_blank">Tug-ani</a></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="work-experience">
+                                    <div class="role-exp">
+                                        <p>I had my first and only role in journalism as an Associate Editor at our school paper in college.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="subsection portfolio-container">
+            <div class="portfolio-header">            
+                <h1>Portfolio</h1>
+            </div>
+            <div class="portfolio-group">
+                <div class="portfolio-row">
+                    <div class="project">
+                        
+                    </div>
+                    <div class="project">
+                        
+                    </div>
+                    <div class="project">
+                        
+                    </div>
+                    <div class="project">
+                        
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+</template>
+
+<script>
+
+</script>
+
+<style></style>
